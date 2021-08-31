3525474d0e468f72366cbf386d5b96d52058bd9b
Rastislav <rasto.kopal@gmail.com>
2021-08-31 23:00:32 +0200

    basic login/register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import Workspace from "./components/Workspace";
-import AppNav from "./components/AppNav"
+// import Workspace from "./components/Workspace";
+import AppNav from "./components/AppNav";
+import {BrowserRouter, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 require('dotenv').config()
 
 function App() {
   return (
     <div className="App">
-      <AppNav />
-      <header className="App-header" class="text-blue-400">
-        Hi TODO list
-      </header>
-      <Workspace />
+      
+      <BrowserRouter>
+        <AppNav />
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </BrowserRouter>
+
+      {/* <Workspace /> */}
     </div>
   );
 }
